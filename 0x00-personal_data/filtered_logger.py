9d436383a876842d4ea459696e6428175152ce2b
kokole12 <kokoleismail886@gmail.com>
2023-08-03 14:12:36 +0300

    checking the validity of the hashed password

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -73,6 +73,7 @@ def get_logger() -> logging.Logger:
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """
+        Creating the connection to the database
     """
     user = os.getenv('PERSONAL_DATA_DB_USERNAME') or "root"
     passwd = os.getenv('PERSONAL_DATA_DB_PASSWORD') or ""

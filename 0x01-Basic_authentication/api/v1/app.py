08f86ec54262863155a65bb47544d6aad372e10a
kokole12 <kokoleismail886@gmail.com>
2023-08-07 23:46:48 +0300

    basic auth class implemented

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -20,6 +20,9 @@ AUTH_TYPE = os.getenv('AUTH_TYPE')
 if AUTH_TYPE == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
+elif AUTH_TYPE == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 
 @app.before_request
@@ -28,12 +31,12 @@ def before_request():
     if auth is None:
         pass
     excluded_path_list = ['/api/v1/status/', '/api/v1/unauthorized/',
-                         '/api/v1/forbidden/']
+                          '/api/v1/forbidden/']
     if auth.require_auth(request.path, excluded_path_list):
-            if auth.authorization_header(request) is None:
-                abort(401, description="Unauthorized")
-            if auth.current_user(request) is None:
-                abort(403, description='Forbidden')
+        if auth.authorization_header(request) is None:
+            abort(401, description="Unauthorized")
+        if auth.current_user(request) is None:
+            abort(403, description='Forbidden')
 
 
 @app.errorhandler(404)

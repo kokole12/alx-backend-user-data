f73ea8fda5e47dee33492e0ed3d4edc238eadd2a
kokole12 <kokoleismail886@gmail.com>
2023-08-08 00:15:55 +0300

    decoding from base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -23,6 +23,7 @@ class BasicAuth(Auth):
     def decode_base64_authorization_header(self,
                                            base64_authorization_header:
                                            str) -> str:
+        """decoding the heading from base64 to ascii code"""
         if base64_authorization_header is None:
             return None
         if not isinstance(base64_authorization_header, str):

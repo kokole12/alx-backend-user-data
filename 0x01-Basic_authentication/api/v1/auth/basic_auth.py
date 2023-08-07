aaf4054004671638f8bc0bb5e11546ad37f670f8
kokole12 <kokoleismail886@gmail.com>
2023-08-08 00:15:52 +0300

    decoding from base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 """Basic authentication implementation"""
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """basic authentication implementation class"""
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
         """function to help extract basic auth header in bytes using base64"""
         if authorization_header is None:
             return None
@@ -17,3 +19,17 @@ class BasicAuth(Auth):
 
         auth_token = authorization_header.split(' ')[-1]
         return auth_token
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            item_to_decode = base64_authorization_header.encode('utf-8')
+            decoded = base64.b64decode(item_to_decode)
+            return decoded.decode('utf-8')
+        except Exception:
+            return None

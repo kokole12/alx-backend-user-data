4d9b16add07e784a5ebeb4eeccb05d35a0d1d5c7
kokole12 <kokoleismail886@gmail.com>
2023-08-07 22:00:12 +0300

    created auth file for tasks 3

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""auth.py file """
+from flask import request
+from typing import TypeVar, List
+
+
+class Auth:
+    """initialising a constructor for the class"""
+    def __init__(self) -> None:
+        pass
+
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """public method that returns false"""
+        return False
+
+
+    def authorization_header(self, request=None) -> str:
+        """setting up the authorization headers"""
+        return None
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """method to returnn the current authorised user"""
+        return None

34e9c96c4f0e5e43c36d9f8b2482a6802c174eb6
kokole12 <kokoleismail886@gmail.com>
2023-08-08 00:03:59 +0300

    basic auth class implemented

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -6,9 +6,7 @@ from typing import TypeVar, List
 
 class Auth:
     """initialising a constructor for the class"""
-    def __init__(self) -> None:
-        pass
-
+    
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """public method that returns false"""
         if path is None:
--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,4 +5,15 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """basic authentication implementation class"""
-    pass
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """function to help extract basic auth header in bytes using base64"""
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith('Basic'):
+            return False
+
+        auth_token = authorization_header.split(' ')[-1]
+        return auth_token

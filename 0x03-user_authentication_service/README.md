c00901365af0a4b940f6512cbcf6d33e21d539ed
kokole12 <kokoleismail886@gmail.com>
2023-08-16 22:42:02 +0300

    solution to task 0

---
+++ 0x03-user_authentication_service/README.md
@@ -0,0 +1,1 @@
+# Alx user data authentication service
---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+    implementing a sqlalchemy model to create the table users
+"""
+from sqlalchemy import String, Integer, Column
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    """implementing the table"""
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_passsword = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)

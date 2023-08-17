61dd501b3a4e5098231e151d9e78b32d71541c11
kokole12 <kokoleismail886@gmail.com>
2023-08-17 11:34:44 +0300

    fixing typo errors

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -7,7 +7,7 @@ from auth import Auth
 
 
 app = Flask(__name__)
-Auth = Auth()
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
@@ -21,7 +21,7 @@ def users() -> str:
     email = request.form.get('email')
     password = request.form.get('password')
     try:
-        user = Auth.register_user(email, password)
+        user = AUTH.register_user(email, password)
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
     return jsonify({"email": f"{email}", "message": "user created"})
@@ -32,7 +32,7 @@ def login() -> str:
     """implementing the user login"""
     email = request.form.get('email')
     password = request.form.get('password')
-    if not Auth.valid_login(email, password):
+    if not AUTH.valid_login(email, password):
         abort(401)
     session_id = Auth.create_session(email)
     resp = jsonify({"email": f"{email}", "message": "logged in"})
@@ -43,7 +43,7 @@ def login() -> str:
 @app.route('/sessions', methods=['DELETE'], strict_slashes=False)
 def logout() -> str:
     session_id = request.cookies.get('session_id', None)
-    user = Auth.get_user_from_session_id(session_id)
+    user = AUTH.get_user_from_session_id(session_id)
 
     if user is None:
         abort(403)
@@ -55,10 +55,22 @@ def logout() -> str:
 def profile() -> str:
     """getting the user profile"""
     session_id = request.cookies.get('session_id')
-    user = Auth.get_user_from_session_id(session_id)
+    user = AUTH.get_user_from_session_id(session_id)
     if user:
         return jsonify({"email": user.email}), 200
     abort(403)
+
+
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """requesting for a password reset"""
+    email = request.form.get('email')
+
+    try:
+        token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        abort(403)
+    return jsonify({"email": email, "reset_token": token})
 
 
 if __name__ == '__main__':
